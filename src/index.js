6740b02246b3c8efdc2a7037f5767d06357d9360
Debangshu Nath <thecrusader.25225@gmail.com>
2026-05-12 16:45:16 +0530

    changed route in indexjs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import express from 'express';
+import modelRoutes from './routes/models.js';
 import cors from 'cors';
-import modelsRouter from './routes/models.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", modelsRouter);
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
+
+app.use('/api', modelRoutes);
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-// test comment 4
